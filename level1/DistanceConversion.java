833d38f09b09b228f84fbb20388116cb7ed9afeb
Your Name <harshil0412singh@gmail.com>
2025-01-10 09:57:01 +0530

    DistanceConversion

---
+++ level1/DistanceConversion.java
@@ -0,0 +1,12 @@
+public class DistanceConversion{
+	public static void main(String[] args){
+	
+	double distanceInKm=10.8;
+	
+	// Convert the distance from kilometers to miles (1 km = 1.6 miles)
+	double distanceInMiles=distanceInKm*1.6;
+	
+	System.out.println("The distance "+distanceInKm+" km in miles is "+distanceInMiles);
+	}
+	
+}
