1e7df2d7d87cd174ae2bc0deed1353ff0277ead5
Your Name <harshil0412singh@gmail.com>
2025-01-10 14:44:17 +0530

    added OddEvenUptoN

---
+++ Level1/OddEvenUptoN.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class OddEvenUptoN{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter a number - ");
+        int number = input.nextInt();
+
+        // Check if the input is a natural number
+        if (number <= 0) {
+			
+            System.out.println("Please enter a natural number");
+			
+        } else {
+			
+            // Iterate from 1 to the number and check oddor even
+			
+            for (int currentNumber = 1; currentNumber <= number; currentNumber++) {
+				
+                if (currentNumber % 2 == 0) {
+					
+
+                    System.out.println(currentNumber + " is an even number");
+
+                } else {
+
+                    System.out.println(currentNumber + " is an odd number");
+
+                }
+            }
+        }
+        input.close();
+    }
+}
