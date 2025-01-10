7904b1876e6384e425a8c49ccb3edc3c6b78dca4
Your Name <harshil0412singh@gmail.com>
2025-01-10 14:41:37 +0530

    added RocketCountDownForLoop

---
+++ Level1/RocketCountDownForLoop.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+class RocketCountDownForLoop{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Get the countdown start value
+        System.out.print("Enter the starting value for countdown - ");
+        int counter = input.nextInt();
+
+        // Performing the countdown
+        for (int i = counter; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        input.close();
+    }
+}
+
