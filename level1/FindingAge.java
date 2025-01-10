5262e17f05a85b7a3132d9227f21132e7e5a0c86
Your Name <harshil0412singh@gmail.com>
2025-01-10 09:55:39 +0530

    FindingAge

---
+++ level1/FindingAge.java
@@ -0,0 +1,16 @@
+public class FindingAge{
+	public static void main(String[] args){
+		
+	String name="Harry";
+	
+	int birthYear=2000;
+	
+	int currentYear=2024;
+	
+	// Calculate the current age by subtracting birthYear from currentYear
+	int currentAge=currentYear-birthYear;
+	
+	System.out.println(name+"'s age in 2024 is "+currentAge);
+	}
+	
+}
