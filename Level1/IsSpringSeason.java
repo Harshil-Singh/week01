92fbf58f1a0cf56f9492f63d4d5ecc6815ec207d
Your Name <harshil0412singh@gmail.com>
2025-01-10 14:39:17 +0530

    added IsSpringSeason

---
+++ Level1/IsSpringSeason.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class IsSpringSeason {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // taking month as input
+        System.out.print("Enter month ");
+        int month = input.nextInt();
+		
+		// taking day as input
+        System.out.print("Enter day ");
+        int day = input.nextInt();
+
+        // Checking if the date is in the Spring season
+        if ((month == 3 && day >= 20 && day <= 31) || 
+            (month == 4) || 
+            (month == 5) || 
+            (month == 6 && day >= 1 && day <= 20)) {
+            System.out.println("It's a Spring Season.");
+        } else {
+            System.out.println("Not a Spring Season.");
+        }
+
+        input.close();
+    }
+}
